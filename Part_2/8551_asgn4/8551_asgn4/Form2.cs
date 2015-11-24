f454dc6c90a62fd6bc05a266faf82dc8a3fb432f
mattku <mattkuu@gmail.com>
2015-11-24 15:45:44 -0800

    Question 4 to 6 stuff

---
+++ Part_2/8551_asgn4/8551_asgn4/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8551_asgn4
+{
+    public partial class Form2 : Form
+    {
+        public Image Image
+        {
+            set { pictureBox1.Image = value; }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }       
+    }
+}
