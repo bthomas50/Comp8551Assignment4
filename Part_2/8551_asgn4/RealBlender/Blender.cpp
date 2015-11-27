d86f80ec210e4d73a65e274ae94e8bcdb4154a5e
unknown <a00861166@SE12L0303-23.ad.bcit.ca>
2015-11-26 17:14:27 -0800

    fixed comments

--- Part_2/8551_asgn4/RealBlender/Blender.cpp
+++ Part_2/8551_asgn4/RealBlender/Blender.cpp
@@ -58,30 +58,31 @@ EXTERN_C __declspec(dllexport) int __stdcall Blend_MMX(MyImage* original, MyImag
 		unsigned char* blend = (unsigned char*)blendTo->linearData + (y * blendTo->stride);
 		for (int x = 0; x < blendTo->width; x += 2)
 		{
-			//FIRST AND SECOND PIXELS!
-			__m64 src_data = *reinterpret_cast<__m64*>(src); //src_data = s(RGBA RGBA RGBA RGBA)
-			__m64 blend_data = *reinterpret_cast<__m64*>(blend); //blend_data = b(RGBA RGBA RGBA RGBA)
+			//FIRST  PIXEL!
+			__m64 src_data = *reinterpret_cast<__m64*>(src); //src_data = s(RGBA RGBA)
+			__m64 blend_data = *reinterpret_cast<__m64*>(blend); //blend_data = b(RGBA RGBA)
 
-			__m64 src_unpacked = _mm_unpacklo_pi8(src_data, zero); //src_unpacked = s(0R 0G 0B 0A 0R 0G 0B 0A)
-			__m64 blend_unpacked = _mm_unpacklo_pi8(blend_data, zero); //blend_unpacked = b(0R 0G 0B 0A 0R 0G 0B 0A)
-			src_unpacked = _mm_sub_pi16(src_unpacked, blend_unpacked); //src_unpacked = s-b(RR GG BB AA RR GG BB AA)
-			blend_unpacked = _mm_slli_pi16(blend_unpacked, 8); //b(R0 G0 B0 A0 R0 G0 B0 A0)
+			__m64 src_unpacked = _mm_unpacklo_pi8(src_data, zero); //src_unpacked = s(0R 0G 0B 0A)
+			__m64 blend_unpacked = _mm_unpacklo_pi8(blend_data, zero); //blend_unpacked = b(0R 0G 0B 0A)
+			src_unpacked = _mm_sub_pi16(src_unpacked, blend_unpacked); //src_unpacked = s-b(RR GG BB AA)
+			blend_unpacked = _mm_slli_pi16(blend_unpacked, 8); //b(R0 G0 B0 A0)
 			src_unpacked = _mm_mullo_pi16(src_unpacked, alpha);
-			__m64 result_first = _mm_add_pi16(blend_unpacked, src_unpacked); //result = RR GG BB AA RR GG BB AA
-			result_first = _mm_srli_pi16(result_first, 8);//result = 0R 0G 0B 0A 0R 0G 0B 0A
-			//THIRD AND FOURTH PIXELS!
-			src_unpacked = _mm_unpackhi_pi8(src_data, zero); //src_unpacked = s(0R 0G 0B 0A 0R 0G 0B 0A)
-			blend_unpacked = _mm_unpackhi_pi8(blend_data, zero); //blend_unpacked = b(0R 0G 0B 0A 0R 0G 0B 0A)
-			src_unpacked = _mm_sub_pi16(src_unpacked, blend_unpacked); //src_unpacked = s-b(RR GG BB AA RR GG BB AA)
-			blend_unpacked = _mm_slli_pi16(blend_unpacked, 8); //b(R0 G0 B0 A0 R0 G0 B0 A0)
+			__m64 result_first = _mm_add_pi16(blend_unpacked, src_unpacked); //result = RR GG BB AA
+			result_first = _mm_srli_pi16(result_first, 8);//result = 0R 0G 0B 0A
+			//SECOND PIXEL!
+			src_unpacked = _mm_unpackhi_pi8(src_data, zero); //src_unpacked = s(0R 0G 0B 0A)
+			blend_unpacked = _mm_unpackhi_pi8(blend_data, zero); //blend_unpacked = b(0R 0G 0B 0AA)
+			src_unpacked = _mm_sub_pi16(src_unpacked, blend_unpacked); //src_unpacked = s-b(RR GG BB AA)
+			blend_unpacked = _mm_slli_pi16(blend_unpacked, 8); //b(R0 G0 B0 A0)
 			src_unpacked = _mm_mullo_pi16(src_unpacked, alpha);
-			__m64 result_second = _mm_add_pi16(blend_unpacked, src_unpacked); //result = RR GG BB AA RR GG BB AA
-			result_second = _mm_srli_pi16(result_second, 8);//result = 0R 0G 0B 0A 0R 0G 0B 0A
+			__m64 result_second = _mm_add_pi16(blend_unpacked, src_unpacked); //result = RR GG BB AA
+			result_second = _mm_srli_pi16(result_second, 8); //result = 0R 0G 0B 0A
+			//SECOND PIXEL!
 			__m64 big_result = _m_packuswb(result_first, result_second);
 			*reinterpret_cast<__m64*>(dst) = big_result;
-			dst += 8; //4 pixels at a time.
-			src += 8; //4 pixels at a time.
-			blend += 8; //4 pixels at a time.
+			dst += 8; //2 pixels at a time.
+			src += 8; //2 pixels at a time.
+			blend += 8; //2 pixels at a time.
 		}
 	}
 	//_mm_empty();
